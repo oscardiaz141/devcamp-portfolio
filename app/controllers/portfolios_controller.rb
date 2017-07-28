3b22ad6e217f9268616464da08021a7b8b85559e
Jordan Hudgens <jordan@devcamp.com>
2017-07-28 18:54:44 +0000

    Implemented partails with local data for portfolio form and nav bars

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -1,5 +1,5 @@
 class PortfoliosController < ApplicationController
-  layout "portfolio"
+  layout 'portfolio'
   
   def index
     @portfolio_items = Portfolio.all
@@ -28,6 +28,7 @@ class PortfoliosController < ApplicationController
 
   def edit
     @portfolio_item = Portfolio.find(params[:id])
+
   end
 
   def update
